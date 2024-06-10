e6baaf6f57428c329c5a317646a037294d88fe45
kailash <mr.kailashssahu08@gmail.com>
2024-06-10 21:38:28 +0530

    hotel reservation commited for second time

---
+++ app/Filament/Pages/EditHotels.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Pages\Page;
+
+class EditHotels extends Page implements HasForms
+{
+    use InteractsWithForms;
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.edit-hotels';
+}
